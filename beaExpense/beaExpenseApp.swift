c437a9a2b44e5937733da11e74b13e6592ec6456
Hieu X. Leu <hieuleuxuan@gmail.com>
2023-04-04 11:36:25 +1000

    Initial Commit

---
+++ beaExpense/beaExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  beaExpenseApp.swift
+//  beaExpense
+//
+//  Created by Hieu X. Leu on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct beaExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
